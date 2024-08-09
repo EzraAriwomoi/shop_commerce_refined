058eb33ca009e0ba6a6edcf6f76015bda0c2c57d
Ezra Ariwomoi <kropezra@gmail.com>
2024-08-09 23:43:03 +0300

    comment remove

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -4,10 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Pages
-// import CustomerAuth from "./pages/customerauth";
-// import MerchantAuth from "./pages/merchantauth";
 import HomePage from "./pages/homepage";
-// import NavBar from "./components/layout/NavBar";
 import ProductDetails from "./pages/ProductDetails";
 import ShoppingcartPage from "./pages/shoppingcart";
 import ProductListPage from "./pages/productlisting";
